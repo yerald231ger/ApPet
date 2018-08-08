6251ea9ae56eb544e0be6bcd6fd617d9d2970d01
Gerardo Sánchez Hernández <yerald231ger@live.com>
2018-08-08 13:05:58 -0500

    optimizacion de codigo en clase ChallengeResult

--- ApPetWeb/Results/ChallengeResult.cs
+++ ApPetWeb/Results/ChallengeResult.cs
@@ -24,8 +24,10 @@ namespace ApPetWeb.Results
         {
             Request.GetOwinContext().Authentication.Challenge(LoginProvider);
 
-            HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.Unauthorized);
-            response.RequestMessage = Request;
+            var response = new HttpResponseMessage(HttpStatusCode.Unauthorized)
+            {
+                RequestMessage = Request
+            };
             return Task.FromResult(response);
         }
     }
